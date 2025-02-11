405a80654a80b57236650e9b2d85d03b3f6049a7
Asim Rauf <idmery77@gmail.com>
2025-02-11 18:20:24 +0500

    Fix script issues for next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
-import { getTranslations } from '@/utils/translations'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const translations = getTranslations('nl')
   return <Component {...pageProps} />
 }
 
