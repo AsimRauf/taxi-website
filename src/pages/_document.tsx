3747d52c9dabbca4e759cc587a0c105f786ad737
Asim Rauf <idmery77@gmail.com>
2025-02-11 18:08:37 +0500

    Fix ESLint errors and script placement

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Montserrat:wght@600;700;800&display=swap" rel="stylesheet" />
+        <Script
+          src="your-script-source"
+          strategy="beforeInteractive"
+        />
       </Head>
       <body>
         <Main />
